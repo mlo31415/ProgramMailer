c4b06a8de80d501f10c3a084a84e648fb782b8bf
mlo31415 <mlo@baskerville.org>
2022-03-03 20:40:51 -0500

    Basic code for parsing the ProgramAnalyzer output to ready for email.

--- ProgramMailer.py
+++ ProgramMailer.py
@@ -1,10 +1,15 @@
 from __future__ import annotations
 
+from HelpersPackage import FindAnyBracketedText
+
 def main():
 
     # Open the schedule markup file
-    with open("../ProgramAnalyzer/Program participant schedules markup.txt", "r") as file:
-        markuplines=file.readlines()
+    markuplines=""
+    with open("../ProgramAnalyzer/reports/Program participant schedules markup.txt", "r") as file:
+        markuplines=file.read()
+    # Remove newlines *outside* markup
+    markuplines=markuplines.replace(">\n<", "><")
 
     # <person>xxxx</person>
     # <person>xxxx</person>
@@ -24,15 +29,61 @@ def main():
     # <precis>yyyyy</precis>
 
     # Markup is a dict keyed by the <name></name> with contents the contained markup and rooted at "main"
-    markup: list[dict|list[str]]=resolve(markuplines)
+    markup: list[tuple[str, str|list]]=resolve([("Main", markuplines)])
+
+    # Now we have the whole schmeer in a hierarchical structure
+    # Generate the output
+    with open("Program participant schedules email.txt", "w") as file:
+        main=markup[0][1]
+        for person in main:
+            fullname=""
+            email=""
+            items=""
+            for attribute in person[0][1]:
+                if attribute[0][0] == "email":
+                    email=attribute[0][1]
+                if attribute[0][0] == "fullname":
+                    fullname=attribute[0][1]
+                if attribute[0][0] == "item":
+                    title=""
+                    participants=""
+                    precis=""
+                    for subatt in attribute[0][1]:
+                        if subatt[0][0] == "title":
+                            title=subatt[0][1]
+                        if subatt[0][0] == "participants":
+                            participants=subatt[0][1]
+                        if subatt[0][0] == "precis":
+                            precis=subatt[0][1]
+                    item=f"{title}\n{participants}\n"
+                    if len(precis) > 0:
+                        item+=f"{precis}\n"
+                    items=items+item+"\n"
+            print(f"\n\n\nDear {fullname}\n{email}\n\nHere's yer schedule:\n{items}", file=file)
 
 
-
-def resolve(markup: list[str]) -> list[dict]:
+# Recursively parse the markup
+def resolve(markup: list[tuple[str, str]]) -> list[tuple[str, list[tuple[str, str]]]]:
     # Replace the list of strings with a list of dicts for each xxx and then call resolve for each of those
     # Find the first (perhaps only) markup in the list of strings
-    i=0
-    return []
+
+    assert len(markup) == 1
+    key=markup[0][0]
+    text=markup[0][1]
+    out=[]
+    while len(text) > 0:
+        lead, bracket, contents, trail=FindAnyBracketedText(text)
+        if bracket == "" and contents == "":
+            if trail != "":
+                # print(f"[({key}, {trail})]")
+                return [(key, trail)]
+            if trail == "":
+                break
+        out.append(resolve([(bracket, contents)]))
+        text=trail
+
+    # print(f"[({key}, {len(out)=})]")
+    return [(key, out)]
 
 
 if __name__ == "__main__":
