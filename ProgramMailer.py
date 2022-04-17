c4163e24c091d7ccad5a554fc5003dc2d5009eae
mlo31415 <mlo@baskerville.org>
2022-04-17 11:12:04 -0400

    Batch of changes to improve error messages and make parameters.txt file more flexible and shared between the three programs

--- ProgramMailer.py
+++ ProgramMailer.py
@@ -1,20 +1,33 @@
 from __future__ import annotations
+from typing import Optional
 
 import smtplib
+import os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-from HelpersPackage import FindAnyBracketedText, MessageLog, SelectFileBasedOnDebugger
+from HelpersPackage import FindAnyBracketedText, MessageLog, ReadListAsParmDict
+
+
+# Search for a Program file and return its path.
+# Look first in the location specified by path.  Failing that, look in defaultDir.  Failing that look in the CWD.
 
 def main():
+    parameters=ReadListAsParmDict('parameters.txt')
+    if parameters is None or len(parameters) == 0:
+        MessageLog(f"Can't open/read {os.getcwd()}/parameters.txt\nProgramMailer terminated.")
+        exit(999)
 
     # Open the mail file which was created using ProgramMailerAssembler
-    allEmails=""
-    with open(SelectFileBasedOnDebugger("../ProgramMailAssembler", "Program participant schedules email.txt"), "r") as file:
+    allemailsPath=OpenProgramFile("Program participant schedules email.txt", parameters["PMADirectory"], ".")
+    if not allemailsPath:
+        MessageLog(f"Can't find 'Program participant schedules email.txt'\nProgramMailer terminated.")
+        exit(999)
+    with open(allemailsPath, "r") as file:
         allEmails=file.read()
 
     # This file contains one or more emails.
-    # An individual email is structures as:
+    # An individual email's structure is:
     #   <email>
     #       <email-address>email address to mail to</email-address>
     #       <content>
@@ -29,22 +42,22 @@ def main():
     while len(allEmails) > 0:
         _, tag, content, remainder=FindAnyBracketedText(allEmails)
         if tag != "email-message":
-            MessageLog(f"Top level tag<{tag}> encountered -- <email> expected.")
+            MessageLog(f"Top level tag<{tag}> encountered -- <email> expected.\nProgramMailer ended.")
             return
         allEmails=remainder.strip()
 
         # Get the email address and message
         _, tag1, emailaddress, remainder=FindAnyBracketedText(content)
         if tag1 != "email-address":
-            MessageLog(f"Top level tag<{tag}> encountered -- <email-address> expected.")
+            MessageLog(f"Top level tag<{tag}> encountered -- <email-address> expected.\nProgramMailer ended.")
             return
         _, tag2, message, _=FindAnyBracketedText(remainder)
         if tag2 != "content":
-            MessageLog(f"Top level tag<{tag}> encountered -- <content> expected.")
+            MessageLog(f"Top level tag<{tag}> encountered -- <content> expected.\nProgramMailer ended.")
             return
         _, tag3, subject, body=FindAnyBracketedText(message)
         if tag3 != "email subject":
-            MessageLog(f"email subject tag<{tag}> missing from <email message>.")
+            MessageLog(f"email subject tag<{tag}> missing from <email message>.\nProgramMailer ended.")
             return
         # sender_pass='moqwjlrbhoisqvkc'  # 'Prog3Analyzer'
         Mail('programtest2022@gmail.com', 'moqwjlrbhoisqvkc', emailaddress, subject, body)
@@ -65,6 +78,34 @@ def Mail(senderAddress: str, password: str, recipient: str, subject: str, conten
     text = message.as_string()
     session.sendmail(senderAddress, recipient, text)
     session.quit()
+
+def OpenProgramFile(fname: str, path: str, defaultDir: str, report=True) -> Optional[str]:
+    if fname is None:
+        MessageLog(f"OpenProgramFile: fname is None, {path=}")
+        return None
+
+    if path is not None:
+        pathname=os.path.join(path, fname)
+        if os.path.exists(pathname):
+            return pathname
+
+    pathname=os.path.join(defaultDir, fname)
+    if os.path.exists(pathname):
+        return pathname
+
+    if os.path.exists(fname):
+        return fname
+
+    if report:
+        if defaultDir != "." and path != ".":
+            MessageLog(f"Can't find '{fname}': checked '{path}', '{defaultDir}' and './'")
+        elif path != ".":
+            MessageLog(f"Can't find '{fname}': checked '{path}' and './'")
+        else:
+            MessageLog(f"Can't find '{fname}': checked './'")
+
+    return None
+
 
 
 if __name__ == "__main__":
